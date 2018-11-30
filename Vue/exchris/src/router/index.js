f64f2a87b0496ed35175e0c83d8511c24cb1c819
exchris <dev.erxuan@gmail.com>
2018-11-30 14:30:03 +0800

    table all select

--- Vue/exchris/src/router/index.js
+++ Vue/exchris/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Table from '@/components/page/table'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/table',
+            name: 'table',
+            component: Table
+        }
+    ]
 })
